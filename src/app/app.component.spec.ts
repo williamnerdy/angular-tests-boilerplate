1c7c7445f227e9beaa75292a31a73b930beac482
William Cunha <william.cunha@iteris.com.br>
2021-03-07 15:55:11 -0300

    Use counter and move style to css file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        CounterComponent
       ],
     }).compileComponents();
   });
